493d62100ed6372e39f56e1155c270f358afd620
michaelsnow3 <mikesnow444@gmail.com>
2019-03-01 13:37:07 -0500

    set up client only api access

--- spotter-server/search/search-song.js
+++ spotter-server/search/search-song.js
@@ -6,5 +6,40 @@ var cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
+var app = express();
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
+
+function search(song){
+  // your application requests authorization
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+
+      // use the access token to access the Spotify Web API
+      var token = body.access_token;
+      var options = {
+        url: `https://api.spotify.com/v1/search?q=Surrender&type=track`,
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        json: true
+      };
+      request.get(options, function(error, response, body) {
+        console.log(body.tracks.items.forEach(track => console.log(track.name)));
+      });
+    }
+  });
+}
+
+export{search}
