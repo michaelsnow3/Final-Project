af7efff725fe2323e1aa99182d6499b394a63a49
michaelsnow3 <mikesnow444@gmail.com>
2019-03-01 18:14:46 -0500

    seperated spotify auth paths into seperate files

--- spotter-server/server.js
+++ spotter-server/server.js
@@ -1,5 +1,31 @@
-const search = require('./search/search-song.js');
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+var querystring = require('querystring');
 
-search.getSongs('everybody wants to rule the world').then(data => {
-  console.log(data[0].artists)
-})
+require('dotenv').config();
+
+var client_id = process.env.CLIENT_ID; // Your client id
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+var redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+   .use(cors())
+   .use(cookieParser());
+
+const loginRoutes = require('./spotify-auth/login-routes.js')
+app.use('/login/', loginRoutes(stateKey, querystring, redirect_uri))
+
+const callbackRoutes = require('./spotify-auth/callback-routes.js');
+app.use('/callback/', callbackRoutes(stateKey, querystring, redirect_uri, request))
+
+const refreshTokenRoutes = require('./spotify-auth/refresh-token-routes.js')
+app.use('/refreshTokenRoutes/', refreshTokenRoutes())
+
+console.log('Listening on 8888');
+app.listen(8888);
