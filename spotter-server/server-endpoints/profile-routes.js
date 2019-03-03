9d09a5c3497f1a2df896bf5f6044e4b6413ac215
michaelsnow3 <mikesnow444@gmail.com>
2019-03-02 19:09:20 -0500

    added server enpoints that allow user to enter a track or artist and search spotify for user input

---
+++ spotter-server/server-endpoints/profile-routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const profileEditRoutes = express.Router();
+const search = require("../search/search-spotify.js");
+
+module.exports = function() {
+  profileEditRoutes.post('/edit/:type', function(req, res) {
+    let inputType = req.params.type;
+    let inputTitle = req.body.title;
+
+    search.searchSpotify(inputType, inputTitle).then(searchList => {
+      console.log(searchList);
+    })
+
+    res.redirect('/')
+  });
+
+  return profileEditRoutes
+
+}
--- spotter-server/server.js
+++ spotter-server/server.js
@@ -1,44 +1,49 @@
-
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const ENV = process.env.ENV || "development";
 const knexConfig = require("./knexfile");
 const knex = require("knex")(knexConfig[ENV]);
 
-const express = require('express'); // Express web server framework
-const request = require('request'); // "Request" library
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const querystring = require('querystring');
+const express = require("express"); // Express web server framework
+const request = require("request"); // "Request" library
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const querystring = require("querystring");
 
 const client_id = process.env.CLIENT_ID; // Your client id
 const client_secret = process.env.CLIENT_SECRET; // Your secret
-const redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+const redirect_uri = `http://localhost:8888/callback/`; // Your redirect uri
 
-const stateKey = 'spotify_auth_state';
+const stateKey = "spotify_auth_state";
 
 const app = express();
+var bodyParser = require('body-parser')
 
-app.use(express.static(__dirname + '/public'))
-   .use(cors())
-   .use(cookieParser());
+app
+  .use(express.static(__dirname + "/public"))
+  .use(cors())
+  .use(cookieParser())
+  .use( bodyParser.json() )      
+  .use(bodyParser.urlencoded({  
+    extended: true
+  })); 
+// spotify Oauth endpoints
+const loginRoutes = require("./server-endpoints/spotify-auth/login-routes.js");
+app.use("/login/", loginRoutes(stateKey, querystring, redirect_uri));
 
-// //spotify Oauth endpoints
-const loginRoutes = require('./spotify-auth/login-routes.js')
-app.use('/login/', loginRoutes(stateKey, querystring, redirect_uri))
+const callbackRoutes = require("./server-endpoints/spotify-auth/callback-routes.js");
+app.use(
+  "/callback/",
+  callbackRoutes(stateKey, querystring, redirect_uri, request)
+);
 
-const callbackRoutes = require('./spotify-auth/callback-routes.js');
-app.use('/callback/', callbackRoutes(stateKey, querystring, redirect_uri, request))
+const refreshTokenRoutes = require("./server-endpoints/spotify-auth/refresh-token-routes.js");
+app.use("/refreshToken/", refreshTokenRoutes());
 
-const refreshTokenRoutes = require('./spotify-auth/refresh-token-routes.js')
-app.use('/refreshTokenRoutes/', refreshTokenRoutes())
+// user profile endpoint
+const profileEditRoutes = require("./server-endpoints/profile-routes.js");
+app.use("/profile/", profileEditRoutes())
 
-const search = require('./search/search-spotify.js')
-search.searchSpotify('track', 'Danger Zone').then(data => {
-  console.log(data)
-})
-
-console.log('Listening on port' + PORT);
-app.listen(PORT);
-
+console.log("Listening on port 8888");
+app.listen(8888);
