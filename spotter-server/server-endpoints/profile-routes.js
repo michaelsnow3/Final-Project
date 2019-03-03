fe93b57b390fe4fd13b678044347561e087b2d17
michaelsnow3 <mikesnow444@gmail.com>
2019-03-02 19:47:54 -0500

    added check for avatar change in edit profile endpoint

--- spotter-server/server-endpoints/profile-routes.js
+++ spotter-server/server-endpoints/profile-routes.js
@@ -6,11 +6,20 @@ module.exports = function() {
   profileEditRoutes.post("/edit/:type", function(req, res) {
     let inputType = req.params.type;
 
+    // edit username
     if (inputType === "username") {
       res.json({ username: req.body.username });
       return;
     }
 
+    // edit avatar
+    if (inputType === "avatar") {
+      res.json({ avatar: req.body.avatar });
+      return;
+    }
+
+
+    // edit favourite song/artist
     let inputTitle = req.body.title;
 
     search.searchSpotify(inputType, inputTitle).then(searchList => {
