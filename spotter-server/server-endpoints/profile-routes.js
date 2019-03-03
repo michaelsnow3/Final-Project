1a4c72962d30d0c2bcaeeb8d96c182d07b1b78f6
michaelsnow3 <mikesnow444@gmail.com>
2019-03-02 19:40:28 -0500

    added check for username change in edit profile enpoint

--- spotter-server/server-endpoints/profile-routes.js
+++ spotter-server/server-endpoints/profile-routes.js
@@ -1,18 +1,23 @@
-const express = require('express');
+const express = require("express");
 const profileEditRoutes = express.Router();
 const search = require("../search/search-spotify.js");
 
 module.exports = function() {
-  profileEditRoutes.post('/edit/:type', function(req, res) {
+  profileEditRoutes.post("/edit/:type", function(req, res) {
     let inputType = req.params.type;
+
+    if (inputType === "username") {
+      res.json({ username: req.body.username });
+      return;
+    }
+
     let inputTitle = req.body.title;
 
     search.searchSpotify(inputType, inputTitle).then(searchList => {
       console.log(searchList);
-      res.json({ searchResults: searchList })
-    })
+      res.json({ searchResults: searchList });
+    });
   });
 
-  return profileEditRoutes
-
-}
+  return profileEditRoutes;
+};
