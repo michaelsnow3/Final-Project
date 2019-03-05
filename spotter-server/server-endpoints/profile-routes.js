b24acbc3d2a0b2ddc1cb0fe98e6556ae9c636958
wang790222 <wang790222@gmail.com>
2019-03-04 21:05:55 -0500

    Showing User Information On Profile Page

--- spotter-server/server-endpoints/profile-routes.js
+++ spotter-server/server-endpoints/profile-routes.js
@@ -3,6 +3,29 @@ const profileEditRoutes = express.Router();
 const search = require("../search/search-spotify.js");
 
 module.exports = function() {
+
+  profileEditRoutes.post("/user_info/:user_token", function(req, res) {
+
+    console.log("user_token:" + params.user_token);
+
+    var options = {
+      url: "https://api.spotify.com/v1/me",
+      headers: { Authorization: "Bearer " + params.user_token },
+      json: true
+    };
+
+    request.get(options, function(error, response, body) {
+      let avatar = (body.images.length === 0) ? null : body.images[0].url;
+      let userInfo = {
+        username: body.display_name,
+        avatar: avatar,
+        email: body.email
+      };
+      console.log(userInfo);
+    });
+    return profileEditRoutes;
+  }
+
   profileEditRoutes.post("/edit/:type", function(req, res) {
     let inputType = req.params.type;
 
