2f17f2b9a43ac18d6fe533a4587572a96a3c2c13
wang790222 <wang790222@gmail.com>
2019-03-07 19:53:58 +0000

    Fix Profile Page Can't Save Data Bug

--- spotter-server/server-endpoints/profile-routes.js
+++ spotter-server/server-endpoints/profile-routes.js
@@ -27,21 +27,30 @@ module.exports = function(request) {
         };
 
         knex('users').where('name', userInfo.name)
-          .then(function(rows) {
-            if (rows.length === 0) {
-              knex('users').insert(userInfo);
-              res.status(200).send(userInfo);
-            } else {
-              _getUserInfoFromDbSendBackApp(userInfo, res);
-            }
-          })
-          .then(() => {
-            console.log("Get User Info");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
+        .then(function(rows) {
+          if (rows.length === 0) {
+            knex('users')
+            .insert(userInfo)
+            .returning('id')
+            .then((id) => {
+              console.log(id[0]);
+              knex('favourite')
+              .insert({"user_id": id[0]})
+              .then(()=> {
+                console.log("Insert New User");
+                res.status(200).send(userInfo);
+              });
+            });
+          } else {
+            _getUserInfoFromDbSendBackApp(userInfo, res);
+          }
+        })
+        .then(() => {
+          console.log("Get User Info");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       } else {
         console.log("profileEditRoutes get error:");
         console.log(error);
