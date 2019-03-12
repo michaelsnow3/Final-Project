3746e2056a07e4b8d27f964f7cb3c6018dd1496b
wang790222 <wang790222@gmail.com>
2019-03-12 05:21:14 +0000

    Fix Add Friend Bug

--- spotter-server/server.js
+++ spotter-server/server.js
@@ -22,7 +22,7 @@ const insertQueries = require("./knexQueries/insertQueries.js")(knex);
 const client_id = process.env.CLIENT_ID; // Your client id
 const client_secret = process.env.CLIENT_SECRET; // Your secret
 
-const redirect_uri = 'http://c514fc0b.ngrok.io/callback/'; // Your redirect uri
+const redirect_uri = 'http://f6753146.ngrok.io/callback/'; // Your redirect uri
 
 const stateKey = "spotify_auth_state";
 
